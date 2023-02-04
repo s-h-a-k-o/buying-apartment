7b4e6a23470928a3ab7a08528601d35386d23ac6
shako0 <chxetiani.95@gmail.com>
2023-02-04 13:53:57 +0400

    adding components

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import { ReactNode } from "react";
+import MainNavigation from "./mainNavigation/mainNavigation";
+
+interface ChildrenType {
+  children: ReactNode;
+}
+
+const Layout: FC<ChildrenType> = ({ children }) => {
+  return (
+    <>
+      <MainNavigation />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/components/layout/mainNavigation/mainNavigation.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const MainNavigation: FC = () => {
+  return <div>MainNavigation</div>;
+};
+
+export default MainNavigation;
