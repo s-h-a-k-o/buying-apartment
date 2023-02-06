697a61c5c9fa3a18d9c8151c93cf06a48e292efa
shako0 <chxetiani.95@gmail.com>
2023-02-06 16:45:45 +0400

    main-nav

--- src/components/layout/mainNavigation/mainNavigation.tsx
+++ src/components/layout/mainNavigation/mainNavigation.tsx
@@ -1,7 +1,71 @@
 import { FC } from "react";
+import { AppBar, Box, Stack, Toolbar, Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Image from "next/image";
+import Link from "next/link";
 
 const MainNavigation: FC = () => {
-  return <div>MainNavigation</div>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      {" "}
+      <AppBar position="static" sx={{ background: "#e8eaf6" }}>
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Box>
+            <Link href="/">
+              <IconButton
+                size="medium"
+                edge="start"
+                aria-label="logo"
+                disableRipple
+              >
+                <Image
+                  src={"/logo.png"}
+                  alt="logo"
+                  width={70}
+                  height={70}
+                  priority
+                />
+                <Typography
+                  variant="h6"
+                  component="div"
+                  sx={{ color: "black", fontWeight: "bold", ml: 2 }}
+                >
+                  Apartments
+                </Typography>
+              </IconButton>
+            </Link>
+          </Box>
+
+          <Box>
+            <Button color="inherit" sx={{ color: "black", fontWeight: "bold" }}>
+              Buy
+            </Button>
+            <Button color="inherit" sx={{ color: "black", fontWeight: "bold" }}>
+              Sell
+            </Button>
+            <Button color="inherit" sx={{ color: "black", fontWeight: "bold" }}>
+              Apartments
+            </Button>
+            <Button color="inherit" sx={{ color: "black", fontWeight: "bold" }}>
+              Contact
+            </Button>
+            <Button
+              variant="outlined"
+              color="inherit"
+              sx={{
+                color: "blue",
+                fontWeight: "bold",
+                ml: 5,
+              }}
+            >
+              Login
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>{" "}
+    </Box>
+  );
 };
 
 export default MainNavigation;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,24 @@
+import Home from "@/components/homePage/home-page";
 import { NextPage } from "next";
-import { Inter } from "@next/font/google";
-import { Roboto } from "@next/font/google";
-import { Box } from "@mui/system";
+// import { Inter } from "@next/font/google";
+// import { Roboto } from "@next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
-const roboto = Roboto({
-  weight: "400",
-  subsets: ["latin"],
-});
+// const inter = Inter({ subsets: ["latin"] });
+// const roboto = Roboto({
+//   weight: "400",
+//   subsets: ["latin"],
+// });
 
-//with tailwindcss
-const interTailwind = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
+// //with tailwindcss
+// const interTailwind = Inter({
+//   subsets: ["latin"],
+//   variable: "--font-inter",
+// });
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <Box bgcolor="red">Material UI Redux Formik Yup Tailwindscc</Box>
-      <div className={inter.className}>HomePage</div>
-      <div className={roboto.className}>HomePage</div>
-      <div className={`${interTailwind.variable} font-sans`}>HomePage</div>
+      <Home />
     </>
   );
 };
